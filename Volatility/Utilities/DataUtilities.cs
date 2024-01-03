52e97e647fd4684fd594151899c0c410b484b65c
Nathan V <jebcraftserver@gmail.com>
2024-01-02 21:19:01 -0500

    Asset & file name Cgs ID endian converter

--- Volatility/Utilities/DataUtilities.cs
+++ Volatility/Utilities/DataUtilities.cs
@@ -11,5 +11,43 @@
         {
             return x64 ? BitConverter.GetBytes(value) : BitConverter.GetBytes((uint)value);
         }
+
+        public static string FlipFileNameEndian(string filePath)
+        {
+            string baseName = Path.GetFileNameWithoutExtension(filePath);
+            string extension = Path.GetExtension(filePath);
+
+            string[] segments = baseName.Split('_');
+
+            FlipAssetEndian(segments);
+
+            return string.Join("_", segments) + extension;
+        }
+
+        public static byte[] FlipAssetEndian(byte[] CgsIDElements)
+        {
+            if (CgsIDElements.Length > 4) // Shouldn't usually happen
+            {
+                Array.Reverse(CgsIDElements, 0, 4);
+            }
+            else
+            {
+                Array.Reverse(CgsIDElements);
+            }
+            return CgsIDElements;
+        }
+
+        public static string[] FlipAssetEndian(string[] CgsIDElements)
+        {
+            if (CgsIDElements.Length > 4) // File names & properties
+            {
+                Array.Reverse(CgsIDElements, 0, 4);
+            }
+            else
+            {
+                Array.Reverse(CgsIDElements);
+            }
+            return CgsIDElements;
+        }
     }
 }
