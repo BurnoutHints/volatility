2c4245461b5551692f50aa6ea3bd7c1399fad407
Nathan V <jebcraftserver@gmail.com>
2024-01-02 21:34:22 -0500

    update test cases

--- Volatility/Frontend.cs
+++ Volatility/Frontend.cs
@@ -1,34 +1,40 @@
 ﻿using Volatility.TextureHeader;
+using Volatility.Utilities;
 
 internal class Frontend
 {
     private static void Main(string[] args)
     {
         /*
-         * Right now, the application simply creates a
-         * texture class akin to what the parser will 
-         * interpret from an input format, then write
-         * it out to a PC (TUB) formatted header file.
+         * Right now, the application simply creates
+         * example texture classes akin to what the parser
+         * will interpret from an input format, then write
+         * them out to various platform formatted header files.
          * 
          * ! = Completed
          * ? = WIP/Needs Testing
          * > = To Do
          * 
-         * TODO LIST!
+         * DONE!
          * --------------------------
          * ! Bit accurate PC header
+         * 
+         * TODO LIST!
+         * --------------------------
          * ? Bit accurate BPR (PC) header (needs testing)
+         * ? Proper x64 header export size (need x64 file reference)
+         * > User input (CLI/GUI?)
          * > Header parsing logic
+         * > PS3/X360 header formats
          * 
          * LOW PRIORITY
          * --------------------------
          * ? Bit accurate BPR headers for other/x64 platforms
          * > Raw DDS texture importing (bundle manager does this)
-         * > PS3/X360 header formats?
          * 
          */
 
-        // Example Texture Data
+        // TUB Texture data test case
         TextureHeaderPC textureHeaderPC = new TextureHeaderPC
         {
             Format = D3DFORMAT.D3DFMT_DXT1,
@@ -39,7 +45,7 @@ internal class Frontend
         };
 
         // Write example texture data to file
-        using (FileStream fs = new FileStream("test_header_PC.bin", FileMode.Create))
+        using (FileStream fs = new FileStream("test_header_PC.dat", FileMode.Create))
         using (BinaryWriter writer = new BinaryWriter(fs))
         {
             textureHeaderPC.WriteToStream(writer);
@@ -47,6 +53,7 @@ internal class Frontend
             fs.Close();
         }
 
+        // BPR Texture data test case
         TextureHeaderBPR textureHeaderBPR = new TextureHeaderBPR
         {
             Format = DXGI_FORMAT.DXGI_FORMAT_BC1_UNORM,
@@ -56,7 +63,8 @@ internal class Frontend
             GRTexture = true
         };
 
-        using (FileStream fs = new FileStream("test_header_BPR.bin", FileMode.Create))
+        // Write 32 bit test BPR header
+        using (FileStream fs = new FileStream("test_header_BPR.dat", FileMode.Create))
         using (BinaryWriter writer = new BinaryWriter(fs))
         {
             textureHeaderBPR.WriteToStream(writer);
@@ -66,12 +74,17 @@ internal class Frontend
 
         textureHeaderBPR.x64Header = true;
 
-        using (FileStream fs = new FileStream("test_header_BPRx64.bin", FileMode.Create))
+        // Write 64 bit test BPR header
+        using (FileStream fs = new FileStream("test_header_BPRx64.dat", FileMode.Create))
         using (BinaryWriter writer = new BinaryWriter(fs))
         {
             textureHeaderBPR.WriteToStream(writer);
             writer.Close();
             fs.Close();
         }
+
+        // File name endian flip test case
+        string endianFlipTestName = "12_34_56_78_texture.dat";
+        Console.WriteLine($"Flipped endian {endianFlipTestName} to {DataUtilities.FlipFileNameEndian(endianFlipTestName)}");
     }
 }
