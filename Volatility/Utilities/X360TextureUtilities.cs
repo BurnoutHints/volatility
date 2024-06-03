29c299282d98815867b03b6d4c4322b9be63d22b
Nathan V. <jebcraftserver@gmail.com>
2024-06-03 13:02:44 -0400

    WIP X360 mipmap repacker

--- Volatility/Utilities/X360TextureUtilities.cs
+++ Volatility/Utilities/X360TextureUtilities.cs
@@ -4,6 +4,126 @@ namespace Volatility.Utilities;
 
 internal class X360TextureUtilities
 {
+    public static void ConvertMipmapsToX360(TextureHeaderBase header, GPUTEXTUREFORMAT format, string inputPath, string outputPath)
+    {
+        using var stream = new FileStream(inputPath, FileMode.Open, FileAccess.Read);
+        using var reader = new BinaryReader(stream);
+
+        int width = header.Width;
+        int height = header.Height;
+        int mipMapCount = header.MipmapLevels;
+        var mipmaps = new byte[mipMapCount][];
+
+        for (int i = 0; i < mipMapCount; i++)
+        {
+            int mipSize = CalculateMipSize(width, height, DataUtilities.CalculatePitchX360((ushort)width, (ushort)height), format);
+            mipmaps[i] = reader.ReadBytes(mipSize);
+
+            width = Math.Max(1, width / 2);
+            height = Math.Max(1, height / 2);
+        }
+
+        var alignedMipmaps = AlignAndPackMipmaps(mipmaps, header, format);
+
+        using var outputStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+        using var writer = new BinaryWriter(outputStream);
+
+        foreach (var mip in alignedMipmaps)
+        {
+            if (mip != null)
+            {
+                writer.Write(mip);
+            }
+        }
+    }
+
+    private static byte[][] AlignAndPackMipmaps(byte[][] mipmaps, TextureHeaderBase textureInfo, GPUTEXTUREFORMAT format)
+    {
+        const int alignment = 4096;
+        var alignedMipmaps = new byte[mipmaps.Length][];
+        int totalSize = 0;
+
+        for (int i = 0; i < mipmaps.Length; i++)
+        {
+            int mipSize = mipmaps[i].Length;
+            int paddedSize = ((mipSize + alignment - 1) / alignment) * alignment;
+
+            alignedMipmaps[i] = new byte[paddedSize];
+            Array.Copy(mipmaps[i], alignedMipmaps[i], mipSize);
+
+            totalSize += paddedSize;
+
+            if (ShouldPackMipLevel(mipmaps[i], format))
+            {
+                alignedMipmaps[i] = PackMipmaps(mipmaps, i, format, out totalSize);
+                for (int j = i + 1; j < mipmaps.Length; j++)
+                {
+                    alignedMipmaps[j] = null;
+                }
+                break;
+            }
+        }
+
+        totalSize = ((totalSize + alignment - 1) / alignment) * alignment;
+
+        return alignedMipmaps;
+    }
+
+    private static bool ShouldPackMipLevel(byte[] mipLevel, GPUTEXTUREFORMAT format)
+    {
+        int tileSize = format switch
+        {
+            GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1_AS_16_16_16_16
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3_AS_16_16_16_16
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5_AS_16_16_16_16
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT5A => 128, // DXT tile size is 128
+            _ => 32, // Uncompressed tile size is 32
+        };
+
+        return mipLevel.Length <= tileSize * tileSize;
+    }
+
+    private static byte[] PackMipmaps(byte[][] mipmaps, int startLevel, GPUTEXTUREFORMAT format, out int totalSize)
+    {
+        const int tileSize = 4096;
+        totalSize = 0;
+
+        for (int i = startLevel; i < mipmaps.Length; i++)
+        {
+            totalSize += mipmaps[i].Length;
+        }
+
+        totalSize = ((totalSize + tileSize - 1) / tileSize) * tileSize;
+        byte[] packedMipmaps = new byte[totalSize];
+
+        int offset = 0;
+        for (int i = startLevel; i < mipmaps.Length; i++)
+        {
+            Array.Copy(mipmaps[i], 0, packedMipmaps, offset, mipmaps[i].Length);
+            offset += mipmaps[i].Length;
+        }
+
+        return packedMipmaps;
+    }
+
+    private static int CalculateMipSize(int width, int height, int pitch, GPUTEXTUREFORMAT format)
+    {
+        return format switch
+        {
+            GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT1_AS_16_16_16_16 => ((width + 3) / 4) * ((height + 3) / 4) * 8, // DXT1: 8 bytes per 4x4 block
+            GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT2_3_AS_16_16_16_16
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT4_5_AS_16_16_16_16
+            or GPUTEXTUREFORMAT.GPUTEXTUREFORMAT_DXT5A => ((width + 3) / 4) * ((height + 3) / 4) * 16, // DXT2/3/4/5: 16 bytes per 4x4 block
+            _ => width * height * pitch,
+        };
+    }
+
     public static void WriteUntiled360TextureFile(TextureHeaderX360 xboxHeader, string textureBitmapPath, string outPath = "")
     {
         if (string.IsNullOrEmpty(outPath))
