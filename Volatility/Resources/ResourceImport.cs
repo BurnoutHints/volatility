979dea33ee6ac2bd4a99742b553bd1577766478c
Nathan V. <jebcraftserver@gmail.com>
2025-07-11 03:33:32 -0400

    implement ReadExternalImport for unpacked resources

--- Volatility/Resources/ResourceImport.cs
+++ Volatility/Resources/ResourceImport.cs
@@ -1,7 +1,61 @@
-﻿namespace Volatility.Resources;
+﻿using YamlDotNet.Serialization;
+
+namespace Volatility.Resources;
 
 public struct ResourceImport
 {
     public ResourceID ReferenceID;
     public bool ExternalImport;
-}
+
+    public static bool ReadExternalImport(byte index, EndianAwareBinaryReader reader, long importBlockOffset, out ResourceImport resourceImport)
+    {
+        resourceImport.ExternalImport = true;
+
+        // In-resource imports block
+        if (reader.BaseStream.Length >= importBlockOffset + (0x10 * index) + 0x10)
+        {
+            long originalPosition = reader.BaseStream.Position;
+            
+            reader.BaseStream.Seek(importBlockOffset + (0x10 * index), SeekOrigin.Begin);
+            
+            resourceImport.ReferenceID = reader.ReadUInt64();
+            
+            reader.BaseStream.Seek(originalPosition, SeekOrigin.Begin);
+            
+            return true;
+        }
+        // YAP imports yaml
+        else if (reader.BaseStream is FileStream fs)
+        {
+            string baseName = Path.GetFileNameWithoutExtension(fs.Name);
+
+            string directory = Path.GetDirectoryName(fs.Name);
+
+            resourceImport.ReferenceID = GetYAMLImportValueAt(Path.Combine(directory, baseName + "_imports.yaml"), index);
+
+            return true;
+        }
+
+        resourceImport = default;
+        return false;
+    }
+
+
+    public static ResourceID GetYAMLImportValueAt(string yamlPath, byte index)
+    {
+        var yaml = File.ReadAllText(yamlPath);
+        var deser = new DeserializerBuilder().Build();
+
+        var list = deser
+            .Deserialize<List<Dictionary<string, string>>>(yaml)
+            ?? throw new InvalidDataException("Expected a YAML sequence of mappings.");
+
+        if (index < 0 || index >= list.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), $"Valid range 0–{list.Count - 1}");
+
+        var kv = list[index].Values.GetEnumerator();
+        kv.MoveNext();
+        return Convert.ToUInt32(kv.Current, 16);
+    }
+};
+
